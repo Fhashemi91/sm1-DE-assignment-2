0b6884f1a227dc9f9f844a323c11deb49895b6a1
Fatemeh Hashemi <62609785+Fhashemi91@users.noreply.github.com>
2021-11-30 08:48:58 +0100

    ingest aws.

---
+++ ingest_aws/app.py
@@ -0,0 +1,39 @@
+import requests
+from google.cloud import storage
+
+storage_client = storage.Client()
+
+
+def get_covid19_cases():
+    """Download covid-19-world cases data from the aws repository. If fails to download, returns None."""
+    url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"
+    resp = requests.get(url)
+    if not resp.status_code == 200:
+        print(f"failed to download the file with status {resp.status_code}")
+        return
+    with open("covid-19-world-cases.csv", "bw") as fileref:
+        fileref.write(resp.content)
+    return resp.status_code
+
+
+def upload_to_bucket(file_name, bucket_name):
+    """Upload a file to desired `bucket_name`"""
+    blob_name = file_name
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename("covid-19-world-cases.csv")
+
+    # returns a public url
+    return blob.public_url
+
+
+def main():
+    bucket_name = "covid19_cases"
+    file_name = "covid-19-world-cases.csv"
+    get_covid19_cases()
+    print("Downloaded covid cases data")
+    upload_to_bucket(file_name, bucket_name)
+
+
+if __name__ == "__main__":
+    main()
